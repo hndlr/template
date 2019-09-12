37793af855ef6ab9c719b886568ecf2e21ba0690
harrytwright <haroldtomwright@gmail.com>
2019-09-12 17:11:04 +0100

    [src] Port build to newer dependencies
    Due to removing command-line-(args|usage) we needed to rebuild with internal ``@harrytwright/cli-config` and using node cheats to call the correct subcommand
    
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

---
+++ src/config/error-list.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const _ = require('lodash');
+const statuses = require('statuses');
+
+const between = require('../utils/between');
+
+let kErrors;
+module.exports = errors();
+
+function errors() {
+  if (kErrors) { return kErrors; }
+
+  kErrors = [];
+  for (const [code, title] of Object.entries(statuses.STATUS_CODES)) {
+    if (between(parseInt(code), 400, 500) || statuses.empty[code] || statuses.redirect[code] || statuses.retry[code]) {
+      kErrors.push({
+        klass: _.upperFirst(_.camelCase(title)),
+        status: parseInt(code)
+      });
+    }
+  }
+  return kErrors;
+}
